5a9b7e261abee4d90833f4d5e44958ab2833d929
Mark Cody <mac110@ads.northwestern.edu>
2017-01-20 15:01:24 -0600

    Friends.js tested and added

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+//DATA - all friends from whom a compatible match is chosen
+
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "ahmed@example.com",
+    scores: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1
+    ]
+  }
+];
